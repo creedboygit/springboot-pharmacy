76278448762b98d2d38673543f29e4ab1c5c7337
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-03-30 19:26:31 +0900

    auditing 클래스 추가

--- src/main/java/com/valletta/pharmacy/pharmacy/entity/Pharmacy.java
+++ src/main/java/com/valletta/pharmacy/pharmacy/entity/Pharmacy.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "pharmacy")
-public class Pharmacy {
+public class Pharmacy extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
