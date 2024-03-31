60139c5006e65177ef2154d9b96d0d9de43ceec1
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-03-31 17:17:18 +0900

    길 찾기와 카카오 주소 검색 서비스 조합

---
+++ src/main/java/com/valletta/pharmacy/pharmacy/service/PharmacyRecommendationService.java
@@ -0,0 +1,38 @@
+package com.valletta.pharmacy.pharmacy.service;
+
+import com.valletta.pharmacy.api.dto.DocumentDto;
+import com.valletta.pharmacy.api.dto.KakaoApiResponseDto;
+import com.valletta.pharmacy.api.service.KakaoAddressSearchService;
+import com.valletta.pharmacy.direction.entity.Direction;
+import com.valletta.pharmacy.direction.service.DirectionService;
+import java.util.List;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PharmacyRecommendationService {
+
+    private final KakaoAddressSearchService kakaoAddressSearchService;
+    private final DirectionService directionService;
+
+    public void recommendPharmacyList(String address) {
+
+        KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
+
+        if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
+            log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input address: {}", address);
+            return;
+        }
+
+        DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().getFirst();
+
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
+
+        directionService.saveAll(directionList);
+    }
+}
