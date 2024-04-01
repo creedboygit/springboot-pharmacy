aaaba2c28d2cfa5bd7f72c7855e1f14bd33ef49a
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-04-01 23:52:04 +0900

    FormController search Controller 추가

--- src/main/java/com/valletta/pharmacy/direction/controller/FormController.java
+++ src/main/java/com/valletta/pharmacy/direction/controller/FormController.java
@@ -1,15 +1,33 @@
 package com.valletta.pharmacy.direction.controller;
 
+import com.valletta.pharmacy.direction.dto.InputDto;
+import com.valletta.pharmacy.pharmacy.service.PharmacyRecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequiredArgsConstructor
 @Controller
 public class FormController {
 
+    private final PharmacyRecommendationService pharmacyRecommendationService;
+
     @GetMapping("/")
     public String index() {
         return "main";
+    }
+
+    @PostMapping("/search")
+    public ModelAndView postDirection(@ModelAttribute InputDto inputDto) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("output");
+        modelAndView.addObject("outputFormList",
+            pharmacyRecommendationService.recommendPharmacyList(inputDto.getAddress()));
+
+        return modelAndView;
     }
 }
