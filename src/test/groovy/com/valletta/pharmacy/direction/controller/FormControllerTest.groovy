5100ad492eba1f08e05df5fc580ec1d64e2d9ed1
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-04-02 21:19:33 +0900

    FormControllerTest 추가

--- src/test/groovy/com/valletta/pharmacy/direction/controller/FormControllerTest.groovy
+++ src/test/groovy/com/valletta/pharmacy/direction/controller/FormControllerTest.groovy
@@ -3,14 +3,14 @@ package com.valletta.pharmacy.direction.controller
 import com.valletta.pharmacy.direction.dto.OutputDto
 import com.valletta.pharmacy.pharmacy.service.PharmacyRecommendationService
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 class FormControllerTest extends Specification {
 
@@ -40,9 +40,30 @@ class FormControllerTest extends Specification {
                 .andExpect(handler().handlerType(FormController.class))
                 .andExpect(handler().methodName("main"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("main"))
+                .andExpect(view().name("main"))
                 .andDo(log())
     }
 
+    def "POST /search"() {
 
+        given:
+        String inputAddress = "서울 성북구 성북동"
+
+        when:
+        def resultActions = mockMvc.perform(
+                post("/search")
+                        .param("address", inputAddress))
+
+        then:
+        1 * pharmacyRecommendationService.recommendPharmacyList(argument -> {
+            assert argument == inputAddress // mock 객체의 argument 검증
+        }) >> outputDtoList
+
+        resultActions
+                .andExpect { status().isOk() }
+                .andExpect { view().name("output") }
+                .andExpect { model().attributeExists("outputformList") } // model에 outputFormList라는 key가 존재하는지 확인
+                .andExpect { model().attribute("outputFormList", outputDtoList) }
+                .andDo { print() }
+    }
 }
