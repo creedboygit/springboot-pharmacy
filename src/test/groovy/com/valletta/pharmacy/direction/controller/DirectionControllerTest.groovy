93b26f5d8d9857cce02ceb61126592fdc24ca789
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-04-04 20:29:59 +0900

    DirectionControllerTest 추가

---
+++ src/test/groovy/com/valletta/pharmacy/direction/controller/DirectionControllerTest.groovy
@@ -0,0 +1,11 @@
+package com.valletta.pharmacy.direction.controller
+
+import com.valletta.pharmacy.direction.service.DirectionService
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+class DirectionControllerTest extends Specification {
+
+    private MockMvc mockMvc
+    private DirectionService directionService = Mock()
+}
